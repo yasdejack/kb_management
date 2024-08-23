9d5930d5619394211672667831014e1fb3772f14
yasdejack <j.shi2@student.vu.nl>
2024-08-23 11:00:53 +0800

    commit changes

--- app01/apps.py
+++ app01/apps.py
@@ -5,9 +5,5 @@ class App01Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app01'
 
-    # def ready(self):
-    #     from .scheduler import start_scheduler
-    #     start_scheduler()
-    #     print("到这了")
 
 
